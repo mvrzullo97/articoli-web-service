87e123ae066f3c77ce241c678d3cf022a45cb628
marzulloa <marzulloa@host.docker.internal>
2024-04-25 10:20:27 +0200

    creazione entità Articoli

--- articoli-web-service/src/main/java/com/betashop/webapp/entities/Articoli.java
+++ articoli-web-service/src/main/java/com/betashop/webapp/entities/Articoli.java
@@ -1,5 +1,50 @@
 package com.betashop.webapp.entities;
 
-public class Articoli {
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "ARTICOLI")
+@Data
+public class Articoli implements Serializable {
+
+	@Id
+	@Column(name = "CODART")
+	private String codArt;
+	
+	@Column(name = "DESCRIZIONE")
+	private String descrizione;
+	
+	@Column(name = "UM")
+	private String um;
+	
+	@Column(name = "CODSTAT")
+	private String codStat;
+	
+	@Column(name = "PZCART")
+	private Integer pzCart;
+	
+	@Column(name = "PESONETTO")
+	private Double pesoNetto;
+	
+	@Column(name = "IDSTATOART")
+	private String idStatoArt;
+	
+	@Column(name = "DATACREAZIONE")
+	@Temporal(TemporalType.DATE)
+	private Date dataCreaz;
+	
+	
+	private static final long serialVersionUID = 7390361014785172285L;
 
 }
